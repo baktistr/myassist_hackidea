bbe6d05f1ea021bacc4db22414aaae9db69ac010
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-06 16:15:08 +0700

    view updated

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -3,206 +3,153 @@
 <!-- begin:: Content -->
 <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
 
-	<!--[html-partial:begin:{"id":"demo4/dist/inc/view/demos/pages/index","page":"index"}]/-->
+  <!--[html-partial:begin:{"id":"demo4/dist/inc/view/demos/pages/index","page":"index"}]/-->
 
-	<!--[html-partial:begin:{"id":"demo4/dist/inc/view/partials/content/dashboards/dashboard-5","page":"index"}]/-->
+  <!--[html-partial:begin:{"id":"demo4/dist/inc/view/partials/content/dashboards/dashboard-5","page":"index"}]/-->
 
-	<!--begin::Dashboard 5-->
+  <!--begin::Dashboard 5-->
 
-	<!--begin::Row-->
-									<div class="row">
-										<div class="col-lg-6 col-xl-4 order-lg-1 order-xl-1">
+  <!--begin::Row-->
+  <div class="row">
+    <div class="col-lg-12 col-xl-8 order-lg-2 order-xl-1">
+      <!--begin::Portlet-->
+      <div class="kt-portlet kt-portlet--height-fluid kt-widget-13">
+        <div class="kt-portlet__body">
+          <div id="kt-widget-slider-13-2" class="kt-slider carousel slide" data-ride="carousel" data-interval="6000">
+            <div class="kt-slider__head">
+              <div class="kt-slider__label">Aset Mapping</div>
+              <div class="kt-slider__nav">
+                <a class="kt-slider__nav-prev carousel-control-prev" href="#kt-widget-slider-13-2" role="button"
+                  data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="kt-slider__nav-next carousel-control-next" href="#kt-widget-slider-13-2" role="button"
+                  data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+                </a>
+              </div>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active kt-slider__body">
+                <div class="kt-widget-13">
+                  <div class="kt-widget-13__body">
+                    <h5 class="text-dark mb-4">Region 1</h5>
+                    <div class="kt-widget-13__desc">
+                      <div class="kt-widget-9__chart">
+                        <!--Doc: For the chart initialization refer to "widgetSalesStatisticsChart" function in "src\theme\app\scripts\custom\dashboard.js" -->
+                        <canvas id="kt_chart_sales_statistics" height="250"></canvas>
+                      </div>
+                    </div>
+                  </div>
 
-											<!--begin::Portlet-->
-											<div class="kt-portlet kt-portlet--height-fluid-half kt-widget ">
-												<div class="kt-portlet__body">
-													<div id="kt-widget-slider-13-1" class="kt-slider carousel slide" data-ride="carousel" data-interval="8000">
-														<div class="text-center kt-slider__head">
-															<div class="text-dark kt-slider__label">Aset Lahan</div>
-														</div>
-														<div class="carousel-inner">
-															<div class=" active kt-slider__body">
-																<div class="kt-widget-13">
-																	<div class="text-center kt-widget-13__body">
-																		<h5>Luas : 1xxxxxx m2</h5>
-																	</div>
-																	
-																</div>
-															</div>
-														
-														</div>
-													</div>
-												</div>
-											</div>
+                </div>
+              </div>
 
-											<!--end::Portlet-->
+            </div>
+          </div>
+        </div>
+      </div>
 
-											<!--begin::Portlet-->
-											<div class="kt-portlet kt-portlet--height-fluid-half kt-widget ">
-												<div class="kt-portlet__body">
-													<div id="kt-widget-slider-13-1" class="kt-slider carousel slide" data-ride="carousel" data-interval="8000">
-														<div class="text-center kt-slider__head">
-															<div class="text-dark kt-slider__label">Aset Bangunan</div>
-														</div>
-														<div class="carousel-inner">
-															<div class=" active kt-slider__body">
-																<div class="kt-widget-13">
-																	<div class="text-center kt-widget-13__body">
-																		<h5>Luas : 1xxxxxx m2</h5>
-																	</div>
-																	
-																</div>
-															</div>
-														
-														</div>
-													</div>
-												</div>
-											</div>
+      <!--end::Portlet-->
+    </div>
 
-											<!--end::Portlet-->
+    <div class="col-lg-6 col-xl-6 order-lg-2 order-xl-1">
 
-											
-											
+      <!--begin::Portlet-->
+      <div class="kt-portlet kt-portlet--height-fluid kt-widget ">
+        <div class="kt-portlet__body">
+          <div id="kt-widget-slider-13-1" class="kt-slider carousel slide" data-ride="carousel" data-interval="8000">
+            <div class="text-center kt-slider__head">
+              <div class="text-dark kt-slider__label">Status Sertifikat Tanah</div>
 
-											
-										</div>
-										<div class="col-lg-12 col-xl-8 order-lg-2 order-xl-1">
+              <!-- button detail -->
+              <div class="kt-portlet__head-toolbar">
+                <div class="kt-portlet__head-toolbar-wrapper">
+                  <div class="dropdown dropdown-inline">
+                    <div class="kt-slider__nav">
+                      <a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-inner">
+              <div class=" active kt-slider__body">
+                <div class="kt-widget-13">
+                  <div class="text-center kt-widget-13__body">
+                    <h5>HGB</h5>
+                    <h5>HGB Jatuh Tempo</h5>
+                    <h5>Hak Pakai</h5>
+                    <h5>Hak Milik</h5>
+                    <h5>Tidak Bersertifikat</h5>
 
-											<!--begin::Portlet-->
-											<div class="kt-portlet kt-portlet--height-fluid kt-widget-13">
-												<div class="kt-portlet__body">
-													<div id="kt-widget-slider-13-2" class="kt-slider carousel slide" data-ride="carousel" data-interval="6000">
-														<div class="kt-slider__head">
-															<div class="kt-slider__label">Aset Mapping</div>
-															<div class="kt-slider__nav">
-																<a class="kt-slider__nav-prev carousel-control-prev" href="#kt-widget-slider-13-2" role="button" data-slide="prev">
-																	<i class="fa fa-angle-left"></i>
-																</a>
-																<a class="kt-slider__nav-next carousel-control-next" href="#kt-widget-slider-13-2" role="button" data-slide="next">
-																	<i class="fa fa-angle-right"></i>
-																</a>
-															</div>
-														</div>
-														<div class="carousel-inner">
-															<div class="carousel-item active kt-slider__body">
-																<div class="kt-widget-13">
-																	<div class="kt-widget-13__body">
-																		<h5 class="text-dark mb-4">Region 1</h5>
-																		<div class="kt-widget-13__desc">
-																			<div class="kt-widget-9__chart">
-																				<!--Doc: For the chart initialization refer to "widgetSalesStatisticsChart" function in "src\theme\app\scripts\custom\dashboard.js" -->
-																				<canvas id="kt_chart_sales_statistics" height="250"></canvas>
-																			</div>
-																		</div>
-																	</div>
-																	
-																</div>
-															</div>
-															
-														</div>
-													</div>
-												</div>
-											</div>
+                  </div>
 
-											<!--end::Portlet-->
-										</div>
+                </div>
+              </div>
 
-										<div class="col-lg-6 col-xl-6 order-lg-2 order-xl-1">
+            </div>
+          </div>
+        </div>
+      </div>
 
-											<!--begin::Portlet-->
-											<div class="kt-portlet kt-portlet--height-fluid kt-widget ">
-												<div class="kt-portlet__body">
-													<div id="kt-widget-slider-13-1" class="kt-slider carousel slide" data-ride="carousel" data-interval="8000">
-														<div class="text-center kt-slider__head">
-															<div class="text-dark kt-slider__label">Status Sertifikat Tanah</div>
-															
-															<!-- button detail -->
-															<div class="kt-portlet__head-toolbar">
-																<div class="kt-portlet__head-toolbar-wrapper">
-																	<div class="dropdown dropdown-inline">
-																	<div class="kt-slider__nav">
-																		<a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
-																			<i class="fa fa-angle-right"></i>
-																		</a>
-																	</div>
-																	</div>
-																</div>	
-															</div>	
-														</div>
-														<div class="carousel-inner">
-															<div class=" active kt-slider__body">
-																<div class="kt-widget-13">
-																	<div class="text-center kt-widget-13__body">
-																		<h5>HGB</h5>
-																		<h5>HGB Jatuh Tempo</h5>
-																		<h5>Hak Pakai</h5>
-																		<h5>Hak Milik</h5>
-																		<h5>Tidak Bersertifikat</h5>
-																		
-																	</div>
-																	
-																</div>
-															</div>
-														
-														</div>
-													</div>
-												</div>
-											</div>
+      <!--end::Portlet-->
+    </div>
+    <div class="col-lg-6 col-xl-6 order-lg-2 order-xl-1">
 
-											<!--end::Portlet-->
-										</div>
-										<div class="col-lg-6 col-xl-6 order-lg-2 order-xl-1">
+      <!--begin::Portlet-->
+      <div class="kt-portlet kt-portlet--height-fluid kt-widget-13">
+        <div class="kt-portlet__body">
+          <div id="kt-widget-slider-13-2" class="col-xs-1 text-center kt-slider carousel slide" data-ride="carousel"
+            data-interval="4000">
+            <div class="kt-slider__head">
+              <div class="text-dark kt-slider__label">Status Riwayat Sengketa</div>
+              <!-- button detail -->
+              <div class="kt-portlet__head-toolbar">
+                <div class="kt-portlet__head-toolbar-wrapper">
+                  <div class="dropdown dropdown-inline">
+                    <div class="kt-slider__nav">
+                      <a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-inner">
+              <div class=" active kt-slider__body">
+                <div class="kt-widget-13">
+                  <div class="kt-widget-13__body">
+                    <h5>Litigasi : 11</h5>
+                    <h5>Non Litigasi : 10</h5>
 
-											<!--begin::Portlet-->
-											<div class="kt-portlet kt-portlet--height-fluid kt-widget-13">
-												<div class="kt-portlet__body">
-													<div id="kt-widget-slider-13-2" class="col-xs-1 text-center kt-slider carousel slide" data-ride="carousel" data-interval="4000">
-														<div class="kt-slider__head">
-															<div class="text-dark kt-slider__label">Status Riwayat Sengketa</div>
-															<!-- button detail -->
-															<div class="kt-portlet__head-toolbar">
-																<div class="kt-portlet__head-toolbar-wrapper">
-																	<div class="dropdown dropdown-inline">
-																	<div class="kt-slider__nav">
-																		<a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
-																			<i class="fa fa-angle-right"></i>
-																		</a>
-																	</div>
-																	</div>
-																</div>	
-															</div>	
-														</div>
-														<div class="carousel-inner">
-															<div class=" active kt-slider__body">
-																<div class="kt-widget-13">
-																	<div class="kt-widget-13__body">
-																		<h5>Litigasi : 11</h5>
-																		<h5>Non Litigasi : 10</h5>
-																		
-																	</div>
-																	
-																</div>
-															</div>
-															
-														</div>
-													</div>
-												</div>
-											</div>
+                  </div>
 
-											<!--end::Portlet-->
-										</div>
+                </div>
+              </div>
 
-										
-										
-									</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-	<!--end::Row-->
+      <!--end::Portlet-->
+    </div>
 
-	<!--end::Dashboard 5-->
 
-	<!--[html-partial:end:{"id":"demo4/dist/inc/view/partials/content/dashboards/dashboard-5","page":"index"}]/-->
 
-	<!--[html-partial:end:{"id":"demo4/dist/inc/view/demos/pages/index","page":"index"}]/-->
+  </div>
+
+  <!--end::Row-->
+
+  <!--end::Dashboard 5-->
+
+  <!--[html-partial:end:{"id":"demo4/dist/inc/view/partials/content/dashboards/dashboard-5","page":"index"}]/-->
+
+  <!--[html-partial:end:{"id":"demo4/dist/inc/view/demos/pages/index","page":"index"}]/-->
 </div>
 
 <!-- end:: Content -->
