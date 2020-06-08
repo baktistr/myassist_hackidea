b718bb0fbfd580c4fbb9c06691edab010bd84bfa
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-09 00:18:19 +0700

    lahan edit

--- app/Views/pages/asetLahan.php
+++ app/Views/pages/asetLahan.php
@@ -65,8 +65,6 @@
                   </span>
                 </div>
               </div>
-              
-              
             </div>
           </div>
           <div class="col-xl-4 order-1 order-xl-2 kt-align-right">
@@ -92,6 +90,128 @@
 
 <!-- end:: Content -->
 
+
+<script>
+  "use strict";
+// Class definition
+
+var lahanDatatable = function () {
+	// Private functions
+
+	// basic demo
+	var demo = function () {
+    var dataSet = JSON.parse('<?= json_encode($lahan) ?>');
+		var datatable = $('#data-lahan').KTDatatable({
+			// datasource definition
+			data: {
+				type: 'local',
+				source: dataSet,
+				pageSize: 10,
+			},
+
+			// layout definition
+			layout: {
+				scroll: true, // enable/disable datatable scroll both horizontal and vertical when needed.
+				footer: false // display/hide footer
+			},
+
+			// column sorting
+			sortable: true,
+
+			pagination: true,
+
+			search: {
+				input: $('#generalSearch')
+			},
+			autoColumns: true,
+
+			// columns definition
+			columns: [
+				{
+					field: 'lahan_id',
+					title: '#',
+					textAlign: 'center',
+				},
+				{
+					field: 'id_areal_fix_old',
+					title: 'ID Areal',
+				},
+				{
+					field: 'nama_lahan',
+					title: 'Nama Lahan',
+				},
+				{
+					field: 'alamat',
+					title: 'Alamat',
+				},
+				{
+					field: 'provinsi_nama',
+					title: 'Provinsi',
+					autoHide: false,
+				},
+				{
+					field: 'kota_nama',
+					title: 'Kota',
+				},
+				{
+					field: 'kecamatan_nama',
+					title: 'Kecamatan'
+				},
+				{
+					field: 'Actions',
+					title: 'Actions',
+					sortable: false,
+					width: 110,
+					overflow: 'visible',
+					autoHide: false,
+					template: function(row) {
+						return '\
+						<div class="dropdown">\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon btn-icon-md" data-toggle="dropdown">\
+                                <i class="la la-cog"></i>\
+                            </a>\
+						  	<div class="dropdown-menu dropdown-menu-right">\
+						    	<a class="dropdown-item" href="#"><i class="la la-edit"></i> Edit Details</a>\
+						    	<a class="dropdown-item" href="#"><i class="la la-leaf"></i> Update Status</a>\
+						    	<a class="dropdown-item" href="#"><i class="la la-print"></i> Generate Report</a>\
+						  	</div>\
+						</div>\
+						<a href="<?= base_url('aset-lahan-edit-') ?>'+row.lahan_id+'" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Edit details">\
+							<i class="la la-edit"></i>\
+						</a>\
+						<a href="<?= base_url('aset-lahan-delete-') ?>'+row.lahan_id+'" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Delete">\
+							<i class="la la-trash"></i>\
+						</a>\
+					';
+					},
+				}
+			],
+		});
+
+    $('#kt_form_status').on('change', function() {
+      datatable.search($(this).val().toLowerCase(), 'status');
+    });
+
+    $('#kt_form_type').on('change', function() {
+      datatable.search($(this).val().toLowerCase(), 'type');
+    });
+
+    $('#kt_form_status,#kt_form_type').selectpicker();
+
+	};
+
+	return {
+		// public functions
+		init: function () {
+			demo();
+		}
+	};
+}();
+
+jQuery(document).ready(function () {
+	lahanDatatable.init();
+});
+</script>
 
 
 <!--end::Page Scripts -->
--- app/Views/pages/asetLahanEdit.php
+++ app/Views/pages/asetLahanEdit.php
@@ -1,9 +1,150 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Page Edit Lahan</title>
-</head>
-<body>
-	Page Edit Lahan
-</body>
-</html>
+<style type="text/css">
+#map {
+  width: "100%";
+  height: 300px;
+}
+</style>
+<?php echo view('./partials/_subheader/subheader-v1.php'); ?>
+
+<!-- begin:: Content -->
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+  <!--begin::Portlet-->
+  <div class="kt-portlet">
+    <div class="kt-portlet__head">
+      <div class="kt-portlet__head-label">
+        <h3 class="kt-portlet__head-title">Data Asset Lahan</h3>
+      </div>
+    </div>
+
+    <!--begin::Form-->
+    <form class="kt-form" action="<?= base_url('/aset-lahan-edit-' . $lahan['lahan_id']);?>" method="post">
+      <div class="kt-portlet__body">
+
+        <div class="form-group">
+          <label>ID Areal</label>
+          <input type="text" class="form-control" name="id_areal_fix_old" placeholder="Enter id areal" value="<?= $lahan['id_areal_fix_old'];?>">
+
+        </div>
+        <div class="form-group">
+          <label>Nama Lahan</label>
+          <input type="text" class="form-control" placeholder="Enter nama lahan" name="nama_lahan" value="<?= $lahan['nama_lahan'];?>">
+        </div>
+        <div class="form-group">
+          <label>Alamat</label>
+          <textarea class="form-control" id="alamatTextArea" rows="3" name="alamat"><?= $lahan['alamat'];?></textarea>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Provinsi</label>
+            <input type="text" class="form-control" placeholder="Enter provinsi" name="provinsi_nama" value="<?= $lahan['provinsi_nama'];?>">
+          </div>
+          <div class="form-group col-md-6">
+            <label>Kota</label>
+            <input type="text" class="form-control" placeholder="Enter nama kota" name="kota_nama" value="<?= $lahan['kota_nama'];?>">
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Kecamatan</label>
+            <input type="text" class="form-control" placeholder="Enter nama kecamatan" name="kecamatan_nama" value="<?= $lahan['kecamatan_nama'];?>">
+          </div>
+          <div class="form-group col-md-6">
+            <label>Desa</label>
+            <input type="text" class="form-control" placeholder="Enter nama desa" name="desa_nama" value="<?= $lahan['desa_nama'];?>">
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <div>
+              <label>Long</label>
+              <input type="text" id="lng" class="form-control" readonly="yes" name="long" value="<?= $lahan['long'];?>">
+            </div>
+            <div>
+              <label>Lat</label>
+              <input type="text" id="lat" class="form-control" readonly="yes" name="lat" value="<?= $lahan['lat'];?>">
+            </div>
+          </div>
+          <div class="form-group col-md-6">
+            <div id="map"></div>
+          </div>
+        </div>
+        <div class="form-group">
+          <label>Luas Tanah</label>
+          <input type="number" class="form-control" name="luas_tanah" value="<?= $lahan['luas_tanah'];?>">
+        </div>
+        <div class="form-group">
+          <label>Luas Gedung</label>
+          <input type="number" class="form-control" name="luas_gedung" value="<?= $lahan['luas_gedung'];?>">
+        </div>
+        <div class="form-group">
+          <label>Penggunaan</label>
+          <input type="text" class="form-control" placeholder="Enter penggunaan" name="penggunaan" value="<?= $lahan['penggunaan'];?>">
+
+        </div>
+        <div class="form-group">
+          <label for="dropdownSertifikat">Sertifikat</label>
+          <select class="form-control" id="dropdownSertifikat" name="sertifikat">
+						<option value="<?= $lahan['sertifikat'];?>">Current: <?= $lahan['sertifikat']; ?></option>
+            <option value="ADA SERTIFIKAT">ADA SERTIFIKAT</option>
+            <option value="TIDAK ADA SERTIFIKAT">TIDAK ADA SERTIFIKAT</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="dropdownTakos">Takos</label>
+          <select class="form-control" id="dropdownTakos" name="takos_t_f">
+						<option value="<?= $lahan['takos_t_f'];?>">Current: <?= $lahan['takos_t_f']; ?></option>
+            <option value="TRUE">TRUE</option>
+            <option value="FALSE">FALSE</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="dropdownPenyimpananSertifikat">Penyimpanan Sertifikat</label>
+          <select class="form-control" id="dropdownPenyimpananSertifikat" name="penyimpanan_sertifikat">
+						<option value="<?= $lahan['penyimpanan_sertifikat'];?>">Current: <?= $lahan['penyimpanan_sertifikat']; ?></option>
+            <option value="TERSIMPAN">TERSIMPAN</option>
+            <option value="TIDAK TERSIMPAN">TIDAK TERSIMPAN</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="dropdownAreaRegional">Area Regional</label>
+          <select class="form-control" id="dropdownAreaRegional" name="area_regional">
+						<option value="<?= $lahan['area_regional'];?>">Current: <?= $lahan['area_regional']; ?></option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label>Kode Provinsi</label>
+          <input type="number" class="form-control" value="<?= $lahan['kode_prov'];?>" name="kode_prov">
+        </div>
+        <div class="form-group">
+          <label>WITEL</label>
+          <input type="text" class="form-control" value="<?= $lahan['witel'];?>" placeholder="Enter penggunaan" name="witel">
+        </div>
+        <div class="form-group">
+          <label>Kode WITEL</label>
+          <input type="number" class="form-control" value="<?= $lahan['kode_witel'];?>" name="kode_witel">
+        </div>
+        <div class="form-group">
+          <label>Kode Lokasi</label>
+          <input type="text" class="form-control" placeholder="Enter kode lokasi (x-xx-xx-xx)" name="kode_lokasi" value="<?= $lahan['kode_lokasi'];?>">
+        </div>
+      </div>
+      <div class="kt-portlet__foot">
+        <div class="kt-form__actions">
+          <button type="submit" class="btn btn-primary">Update</button>
+          <button type="reset" class="btn btn-secondary">Cancel</button>
+        </div>
+      </div>
+    </form>
+
+    <!--end::Form-->
+  </div>
+</div>
+
+<!-- end:: Content -->
