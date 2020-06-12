5c823de7ffe421cc6f18927e94b3e040ff20f0c8
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-13 02:12:33 +0700

    add chart

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -33,7 +33,7 @@
                 <div class=" active kt-slider__body">
                   <div class="kt-widget-13">
                     <div class="text-center kt-widget-13__body">
-                      <h5>Luas: 1xxxxxx m2</h5>
+                      <h5>Luas: <?= $asset_lahan; ?> m2</h5>
                     </div>
                   </div>
                 </div>
@@ -63,7 +63,7 @@
                 <div class=" active kt-slider__body">
                   <div class="kt-widget-13">
                     <div class="text-center kt-widget-13__body">
-                      <h5>Luas: 1xxxxxx m2</h5>
+                      <h5>Luas: <?= $asset_bangunan; ?> m2</h5>
                     </div>
                   </div>
                 </div>
@@ -98,14 +98,12 @@
                     <div class="kt-widget-13__desc">
                       <div class="kt-widget-9__chart">
                         <!--Doc: For the chart initialization refer to "widgetSalesStatisticsChart" function in "src\theme\app\scripts\custom\dashboard.js" -->
-                        <canvas id="kt_chart_sales_statistics" height="250"></canvas>
+                        <canvas id="aset_mapping" height="250"></canvas>
                       </div>
                     </div>
                   </div>
-
                 </div>
               </div>
-
             </div>
           </div>
         </div>
@@ -128,30 +126,18 @@
               <div class="text-dark kt-slider__label">Status Sertifikat Tanah</div>
 
               <!-- button detail -->
-              <div class="kt-portlet__head-toolbar">
-                <div class="kt-portlet__head-toolbar-wrapper">
-                  <div class="dropdown dropdown-inline">
-                    <div class="kt-slider__nav">
-                      <a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+ 
             </div>
             <div class="carousel-inner">
               <div class=" active kt-slider__body">
                 <div class="kt-widget-13">
                   <div class="text-center kt-widget-13__body">
-                    <h5>HGB</h5>
-                    <h5>HGB Jatuh Tempo</h5>
-                    <h5>Hak Pakai</h5>
-                    <h5>Hak Milik</h5>
-                    <h5>Tidak Bersertifikat</h5>
-
+                    <h5>HGB: <?= $status_sertifikat_lahan['hgb'];?></h5>
+                    <h5>HGB Jatuh Tempo: <?= $status_sertifikat_lahan['hgbjt'];?></h5>
+                    <h5>Hak Pakai: <?= $status_sertifikat_lahan['hp'];?> </h5>
+                    <h5>Hak Milik: <?= $status_sertifikat_lahan['hm']; ?> </h5>
+                    <h5>Tidak Bersertifikat: <?= $status_sertifikat_lahan['no_sertifikat']; ?> </h5>
                   </div>
-
                 </div>
               </div>
 
@@ -172,41 +158,132 @@
             <div class="kt-slider__head">
               <div class="text-dark kt-slider__label">Status Riwayat Sengketa</div>
               <!-- button detail -->
-              <div class="kt-portlet__head-toolbar">
-                <div class="kt-portlet__head-toolbar-wrapper">
-                  <div class="dropdown dropdown-inline">
-                    <div class="kt-slider__nav">
-                      <a class="kt-slider__nav-next carousel-control-next" href="#" role="button" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+ 
             </div>
             <div class="carousel-inner">
               <div class=" active kt-slider__body">
                 <div class="kt-widget-13">
                   <div class="kt-widget-13__body">
-                    <h5>Litigasi : 11</h5>
-                    <h5>Non Litigasi : 10</h5>
-
+                    <h5>Litigasi: <?= $status_riwayat_sengketa['litigasi']; ?></h5>
+                    <h5>Non Litigasi: <?= $status_riwayat_sengketa['non_litigasi']; ?> </h5>
                   </div>
-
                 </div>
               </div>
-
             </div>
           </div>
         </div>
       </div>
-
       <!--end::Portlet-->
     </div>
-
-
-
   </div>
+
+  <script>
+  var asetMappingChart = function() {
+  if (!document.getElementById('aset_mapping')) {
+    return;
+  }
+  var asset_mapping = <?= $asset_mapping; ?>
+
+  var color = Chart.helpers.color;
+  var barChartData = {
+    labels: ['Regional 1', 'Regional 2', 'Regional 3', 'Regional 4'],
+    datasets: [{
+      label: 'Sales',
+      backgroundColor: color(KTApp.getStateColor('brand')).alpha(1).rgbString(),
+      borderWidth: 0,
+      data: asset_mapping
+    }]
+  };
+
+  var ctx = document.getElementById('aset_mapping').getContext('2d');
+  var myBar = new Chart(ctx, {
+    type: 'bar',
+    data: barChartData,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: false,
+      scales: {
+        xAxes: [{
+          categoryPercentage: 0.35,
+          barPercentage: 0.70,
+          display: true,
+          scaleLabel: {
+            display: false,
+            labelString: 'Month'
+          },
+          gridLines: false,
+          ticks: {
+            display: true,
+            beginAtZero: true,
+            fontColor: KTApp.getBaseColor('shape', 3),
+            fontSize: 13,
+            padding: 10
+          }
+        }],
+        yAxes: [{
+          categoryPercentage: 0.35,
+          barPercentage: 0.70,
+          display: true,
+          scaleLabel: {
+            display: false,
+            labelString: 'Value'
+          },
+          gridLines: {
+            color: KTApp.getBaseColor('shape', 2),
+            drawBorder: false,
+            offsetGridLines: false,
+            drawTicks: false,
+            borderDash: [3, 4],
+            zeroLineWidth: 1,
+            zeroLineColor: KTApp.getBaseColor('shape', 2),
+            zeroLineBorderDash: [3, 4]
+          },
+          ticks: {
+            max: 70,
+            stepSize: 10,
+            display: true,
+            beginAtZero: true,
+            fontColor: KTApp.getBaseColor('shape', 3),
+            fontSize: 13,
+            padding: 10
+          }
+        }]
+      },
+      title: {
+        display: false
+      },
+      hover: {
+        mode: 'index'
+      },
+      tooltips: {
+        enabled: true,
+        intersect: false,
+        mode: 'nearest',
+        bodySpacing: 5,
+        yPadding: 10,
+        xPadding: 10,
+        caretPadding: 0,
+        displayColors: false,
+        backgroundColor: KTApp.getStateColor('brand'),
+        titleFontColor: '#ffffff',
+        cornerRadius: 4,
+        footerSpacing: 0,
+        titleSpacing: 0
+      },
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 5,
+          bottom: 5
+        }
+      }
+    }
+  });
+  };
+  // end chart 2
+  </script>
 
   <!--end::Row-->
 
--- app/Views/pages/landingPage.php
+++ app/Views/pages/landingPage.php
@@ -8,17 +8,20 @@
   <link href="<?php base_url() ?>assets/bulma/css/bulma.min.css" rel="stylesheet" type="text/css" />
   <link href="<?php base_url() ?>assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet"
     type="text/css" />
+  <link rel="stylesheet" href="<?php base_url() ?>assets/owlcarousel/dist/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?php base_url() ?>assets/owlcarousel/dist/assets/owl.theme.default.min.css">
   <link rel="stylesheet" href="<?php base_url();?>assets/css/mystyles.css">
-  <link rel="icon" type="image/png" href="/favicon.png"/>
-  <link rel="icon" type="image/png" href="<?php base_url();?>assets/media/logos/brand-logo-crop.png"/>
+  <link rel="icon" type="image/png" href="/favicon.png" />
+  <link rel="icon" type="image/png" href="<?php base_url();?>assets/media/logos/brand-logo-crop.png" />
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <a class="navbar-item" href="<?= base_url('home'); ?>">
         <img src="<?php base_url() ?>assets/media/logos/brand-logo-crop.png" class="kt-header__brand-logo-default" />
-        <!-- <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> -->
       </a>
 
       <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
@@ -31,9 +34,11 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-
-        <a class="navbar-item">
+        <a class="navbar-item" href="#documentation">
           Documentation
+        </a>
+        <a class="navbar-item" href="#screenshot">
+          Guide
         </a>
       </div>
 
@@ -41,6 +46,9 @@
         <div class="navbar-item">
           <div class="buttons">
             <a class="button is-primary" href="<?= base_url('/login');?>" target="_blank">
+              <span class="material-icons">
+                login
+              </span>
               Dashboard
             </a>
           </div>
@@ -50,7 +58,8 @@
   </nav>
 
   <main>
-    <section class="hero is-medium is-primary">
+    <section class="hero is-medium is-primary"
+      style="background-image: url(https://static.dezeen.com/uploads/2019/01/citic-tower-beijing-ctbuh-skyscraper-report-2018-hero_a-1704x959.jpg);background-position: center;background-size: cover;">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
@@ -63,14 +72,41 @@
       </div>
     </section>
 
-    <section class="section">
+    <section class="section" style="background-color: white;" id="documentation">
       <div class="container has-text-centered">
         <h1 class="title">API Documentation</h1>
         <h2 class="subtitle">
-          To go to API Documentation, please click button below.
+          To go to API Documentation, please click button below
         </h2>
-        <a href="https://app.swaggerhub.com/apis/mohsulthana/myAssets/1.0.0" target="_blank"
+        <a href="https://app.swaggerhub.com/apis-docs/mohsulthana/myAssets/1.0.0" target="_blank"
           class="button is-primary is-rounded is">OPEN API</a>
+      </div>
+    </section>
+
+    <section class="section screenshot" id="screenshot" style="background-color: #800000;">
+      <div class="container-fluid has-text-centered">
+        <h1 class="title has-text-white">Guide</h1>
+        <h2 class="subtitle has-text-white">
+          Here's some snapshot from the app
+        </h2>
+        <div class="owl-carousel has-text-white">
+          <div>
+            <img src="<?= base_url('assets/img/dashboard-1.png');?>" alt="">
+            <p>Dashboard</p>
+          </div>
+          <div>
+            <img src="<?= base_url('assets/img/dashboard-2.png');?>" alt="">
+            <p>Table Lahan</p>
+          </div>
+          <div>
+            <img src="<?= base_url('assets/img/dashboard-3.png');?>" alt="">
+            <p>Table Bangunan</p>
+          </div>
+          <div>
+            <img src="<?= base_url('assets/img/user-control.png');?>" alt="">
+            <p>User Control</p>
+          </div>
+        </div>
       </div>
     </section>
 
@@ -92,4 +128,24 @@
 .navbar-item img {
   max-height: 3.75rem;
 }
+
+.owl-item img {
+  max-height: 180px;
+}
+
+.owl-next span,
+.owl-prev span {
+  font-size: 3rem;
+  margin: 0 .5rem;
+}
 </style>
+
+<script src="<?php base_url() ?>assets/owlcarousel/dist/owl.carousel.min.js"></script>
+<script>
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    margin: 10,
+    nav: true
+  });
+});
+</script>
