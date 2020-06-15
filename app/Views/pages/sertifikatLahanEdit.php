4a1e8a53332f00c8ec6fdac4d58e4a218b25c662
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-06-15 20:14:09 +0700

    add new lahan in sertifikat & bangunan detail view

--- app/Views/pages/sertifikatLahanEdit.php
+++ app/Views/pages/sertifikatLahanEdit.php
@@ -12,38 +12,46 @@
 
     <!--begin::Form-->
     <?= session()->getFlashdata('msg') ? $_SESSION['msg'] : ''; ?>
-    <form class="kt-form" action="/sertifikat-lahan-edit-<?= $sertifikat['sertifikat_id']; ?>" method="post" enctype="multipart/form-data">
+    <form class="kt-form" action="/sertifikat-lahan-edit-<?= $sertifikat['sertifikat_id']; ?>" method="post"
+      enctype="multipart/form-data">
       <div class="kt-portlet__body">
         <div class="form-group">
           <label>ID Areal</label>
-          <input type="text" class="form-control" placeholder="Enter id areal" name="id_areal" value="<?= $sertifikat['id_areal'];?>">
+          <input type="text" class="form-control" placeholder="Enter id areal" name="id_areal"
+            value="<?= $sertifikat['id_areal'];?>">
         </div>
         <div class="form-group">
           <label>No Sertifikat</label>
-          <input type="text" class="form-control" placeholder="Enter no sertifikat" name="no_sertifikat" value="<?= $sertifikat['no_sertifikat'];?>">
+          <input type="text" class="form-control" placeholder="Enter no sertifikat" name="no_sertifikat"
+            value="<?= $sertifikat['no_sertifikat'];?>">
         </div>
         <div class="form-group">
           <label>Lokasi Aset</label>
-          <textarea class="form-control" id="lokasiAsetTextarea" name="lokasi_aset" rows="3"><?= $sertifikat['lokasi_aset'];?></textarea>
+          <textarea class="form-control" id="lokasiAsetTextarea" name="lokasi_aset"
+            rows="3"><?= $sertifikat['lokasi_aset'];?></textarea>
         </div>
         <div class="form-row">
           <div class="form-group col-md-6">
             <label>Provinsi</label>
-            <input type="text" class="form-control" name="provinsi" placeholder="Enter provinsi" value="<?= $sertifikat['provinsi'];?>">
+            <input type="text" class="form-control" name="provinsi" placeholder="Enter provinsi"
+              value="<?= $sertifikat['provinsi'];?>">
           </div>
           <div class="form-group col-md-6">
             <label>Kodya</label>
-            <input type="text" class="form-control" placeholder="Enter nama kodya" name="kodya" value="<?= $sertifikat['kodya'];?>">
+            <input type="text" class="form-control" placeholder="Enter nama kodya" name="kodya"
+              value="<?= $sertifikat['kodya'];?>">
           </div>
         </div>
         <div class="form-row">
           <div class="form-group col-md-6">
             <label>Kecamatan</label>
-            <input type="text" class="form-control" placeholder="Enter nama kecamatan" name="kecamatan" value="<?= $sertifikat['kecamatan'];?>">
+            <input type="text" class="form-control" placeholder="Enter nama kecamatan" name="kecamatan"
+              value="<?= $sertifikat['kecamatan'];?>">
           </div>
           <div class="form-group col-md-6">
             <label>Kelurahan</label>
-            <input type="text" class="form-control" placeholder="Enter nama kelurahan" name="kelurahan" value="<?= $sertifikat['kelurahan'];?>">
+            <input type="text" class="form-control" placeholder="Enter nama kelurahan" name="kelurahan"
+              value="<?= $sertifikat['kelurahan'];?>">
           </div>
         </div>
         <div class="form-group">
@@ -61,19 +69,23 @@
         </div>
         <div class="form-group">
           <label>WITEL</label>
-          <input type="text" class="form-control" placeholder="Enter WITEL" name="witel" value="<?= $sertifikat['witel'];?>">
+          <input type="text" class="form-control" placeholder="Enter WITEL" name="witel"
+            value="<?= $sertifikat['witel'];?>">
         </div>
         <div class="form-group">
           <label>Luas</label>
-          <input type="number" class="form-control" placeholder="enter luas" name="luas" value="<?= $sertifikat['luas'];?>">
+          <input type="number" class="form-control" placeholder="enter luas" name="luas"
+            value="<?= $sertifikat['luas'];?>">
         </div>
         <div class="form-group">
           <label>SK HAK</label>
-          <input type="text" class="form-control" placeholder="Enter SK HAK" name="sk_hak" value="<?= $sertifikat['sk_hak'];?>">
+          <input type="text" class="form-control" placeholder="Enter SK HAK" name="sk_hak"
+            value="<?= $sertifikat['sk_hak'];?>">
         </div>
         <div class="form-group">
           <label>Jenis Sertifikat</label>
-          <input type="text" class="form-control" placeholder="Enter jenis sertifikat" name="jenis" value="<?= $sertifikat['jenis'];?>">
+          <input type="text" class="form-control" placeholder="Enter jenis sertifikat" name="jenis"
+            value="<?= $sertifikat['jenis'];?>">
         </div>
         <div class="form-group">
           <label for="dateAkhir">Akhir</label>
@@ -93,16 +105,19 @@
         </div>
         <div class="form-group">
           <label>Bundel</label>
-          <input type="text" class="form-control" placeholder="Enter bundel" name="bundel" value="<?= $sertifikat['bundel']; ?>">
+          <input type="text" class="form-control" placeholder="Enter bundel" name="bundel"
+            value="<?= $sertifikat['bundel']; ?>">
         </div>
         <div class="form-group">
           <label>Halaman</label>
-          <input type="text" class="form-control" placeholder="Enter halaman" name="halaman" value="<?= $sertifikat['halaman']; ?>">
+          <input type="text" class="form-control" placeholder="Enter halaman" name="halaman"
+            value="<?= $sertifikat['halaman']; ?>">
         </div>
         <div class="form-group">
           <label for="dropdownStatusPenyimpanan">Status Penyimpanan</label>
           <select class="form-control" id="dropdownStatusPenyimpanan" name="status_penyimpanan">
-            <option value="<?= $sertifikat['status_penyimpanan']; ?>">Current: <?= $sertifikat['status_penyimpanan'];?></option>
+            <option value="<?= $sertifikat['status_penyimpanan']; ?>">Current: <?= $sertifikat['status_penyimpanan'];?>
+            </option>
             <option value="Ada">Ada</option>
             <option value="Tidak Ada">Tidak Ada</option>
           </select>
--- app/Views/pages/sertifikatLahanView.php
+++ app/Views/pages/sertifikatLahanView.php
@@ -2,10 +2,14 @@
 #map {
   width: "100%";
   height: 300px;
-};
+}
+
+;
+
 dt::after {
   content: ":";
 }
+
 </style>
 
 <?php echo view('./partials/_subheader/subheader-v1.php'); ?>
@@ -14,7 +18,7 @@ dt::after {
     <div class="kt-portlet__head kt-portlet__head--lg">
       <div class="kt-portlet__head-label">
         <h3 class="kt-portlet__head-title">
-          Sertifikat <span class="text-danger"><?= $sertifikat['nama_sertifikat']; ?></span>
+          Sertifikat <span class="text-danger"><?= $sertifikat['no_sertifikat']; ?></span>
         </h3>
       </div>
     </div>
@@ -53,3 +57,84 @@ dt::after {
     </div>
   </div>
 </div>
+
+<?php if(isset($lahan)) { ?>
+<div class="kt-container  kt-grid__item kt-grid__item--fluid">
+  <div class="kt-portlet kt-portlet--mobile">
+    <div class="kt-portlet__head kt-portlet__head--lg">
+      <div class="kt-portlet__head-label">
+        <h3 class="kt-portlet__head-title">
+          Lahan <span class="text-danger"><?= $lahan['nama_lahan']; ?></span>
+        </h3>
+      </div>
+    </div>
+    <div class="kt-portlet__body">
+      <div class="row">
+        <div class="col-lg-12">
+          <table class="table table-head-noborder" id="table-lahan">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama Lahan</th>
+                <th>Alamat</th>
+                <th>Provinsi</th>
+                <th>Kota</th>
+                <th>Kecamatan</th>
+                <th>Desa</th>
+                <th>Lat</th>
+                <th>Long</th>
+                <th>Takos</th>
+                <th>Luas Tanah</th>
+                <th>Luas Gedung</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td><?= $lahan['nama_lahan']; ?></td>
+                <td><?= $lahan['alamat']; ?></td>
+                <td><?= $lahan['provinsi_nama']; ?></td>
+                <td><?= $lahan['kota_nama']; ?></td>
+                <td><?= $lahan['kecamatan_nama']; ?></td>
+                <td><?= $lahan['desa_nama']; ?></td>
+                <td><?= $lahan['lat']; ?></td>
+                <td><?= $lahan['long']; ?></td>
+                <td><?= $lahan['takos_t_f']; ?></td>
+                <td><?= $lahan['luas_tanah']; ?></td>
+                <td><?= $lahan['luas_gedung']; ?></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="row">
+
+      </div>
+    </div>
+  </div>
+</div>
+<?php } ?>
+
+<script>
+$(document).ready(function() {
+  var t = $('#table-lahan').DataTable({
+    "columnDefs": [{
+      "searchable": false,
+      "orderable": false,
+      "targets": 0
+    }],
+    "order": [
+      [1, 'asc']
+    ]
+  });
+
+  t.on('order.dt search.dt', function() {
+    t.column(0, {
+      search: 'applied',
+      order: 'applied'
+    }).nodes().each(function(cell, i) {
+      cell.innerHTML = i + 1;
+    });
+  }).draw();
+});
+</script>
